b140999fc07145f25a1978248b0e61d02fdcc081
Johnson <tjtgzxs@outlook.com>
2017-12-23 06:16:21 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','This is About Page')
+@section('content')
+    <h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','This is My Blog')
+@section('content')
+  <h1>主页</h1>
+@stop
