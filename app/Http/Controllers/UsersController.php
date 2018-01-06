28cdc233932ab0f188584e004d99d3a27c3d880c
Johnson <tjtgzxs@outlook.com>
2018-01-06 05:33:32 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -94,4 +94,16 @@ class UsersController extends Controller
         return back();
     }
 
+    public function followings(User $user){
+        $user=$user->followings()->paginate(30);
+        $title='关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user){
+        $users=$user->followers()->paginate(30);
+        $title='粉丝';
+        return view('users.show_follow',compact('users','title'));
+    }
+
 }
